05a012c57c0e44a11f9f1a60dc4b7a16329d75fb
leandrotune <leandro.tune@hotmail.com>
2024-11-11 20:15:00 -0300

    chore: add tailwind prettier plugin

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const home = () => {
+  return (
+    <div>
+      <h1 className="flex items-end justify-center text-red-500">red</h1>
+    </div>
+  );
+};
+
+export default home;
--- app/transactions/page.tsx
+++
@@ -1,5 +0,0 @@
-export const Transactions  = () => {
-  return (
-    <div className=""></div>
-  )
-}
